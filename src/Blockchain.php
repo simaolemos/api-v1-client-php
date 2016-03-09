347571ea7979a1130a96235b8e8b0ffe67779b67
Antoine Le Calvez <antoine@blockchain.com>
2016-03-09 09:57:14 +0000

    Changed URL for calls to service-mywallet

--- src/Blockchain.php
+++ src/Blockchain.php
@@ -43,7 +43,7 @@ if(!function_exists('curl_init')) {
 
 class Blockchain {
     const URL = 'https://blockchain.info/';
-
+    const SERVICE_URL = 'http://localhost';
 
     private $ch;
     private $api_code = null;
@@ -83,7 +83,13 @@ class Blockchain {
     }
 
     public function post($resource, $data=null) {
-        curl_setopt($this->ch, CURLOPT_URL, self::URL.$resource);
+        $url = URL;
+
+        if (($resource == "api/v2/create") || (substr($resource, 0, 8) === "merchant")) {
+            $url = SERVICE_URL;
+        }
+
+        curl_setopt($this->ch, CURLOPT_URL, $url.$resource);
         curl_setopt($this->ch, CURLOPT_POST, true);
 
         curl_setopt($this->ch, CURLOPT_HTTPHEADER, 
@@ -106,6 +112,12 @@ class Blockchain {
     }
 
     public function get($resource, $params=null) {
+        $url = URL;
+
+        if (($resource == "api/v2/create") || (substr($resource, 0, 8) === "merchant")) {
+            $url = SERVICE_URL;
+        }
+
         curl_setopt($this->ch, CURLOPT_POST, false);
 
         if(!is_null($this->api_code)) {
@@ -114,7 +126,7 @@ class Blockchain {
         curl_setopt($this->ch, CURLOPT_HTTPHEADER, array());
 
         $query = http_build_query($params);
-        curl_setopt($this->ch, CURLOPT_URL, self::URL.$resource.'?'.$query);
+        curl_setopt($this->ch, CURLOPT_URL, $url.$resource.'?'.$query);
 
         return $this->_call();
     }
